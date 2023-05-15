7b3a08b4575743b0790f2912891500ebebc556da
romanwixinger <roman.wixinger@gmail.com>
2023-05-15 12:28:24 +0200

    [main] Initial commit.
    Todo:
    - Add files and helpers from old repo.
    - Debug.
    - Test transpilation and run with low number of qubits.
    - Test transpilation with high number of qubits.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='noisy-gates-approach-runs',
+    version='0.1.0',
+    author='M. Grossi, G. D. Bartolomeo, M. Vischi, R. Wixinger',
+    author_email='michele.grossi@cern.ch, dibartolomeo.giov@gmail.com, vischimichele@gmail.com, roman.wixinger@gmail.com',
+    packages=[],
+    license='MIT',
+    python_requires='>=3.9'
+)
